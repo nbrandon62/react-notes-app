71caabf331340d1c48f721fb21258b6160ab2a89
Brandon Nguyen <nbrandon62@gmail.com>
2022-11-10 21:13:44 -0800

    added dark theme toggle

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,25 @@
-import React from 'react'
+import React from "react";
 
-const Header = () => {
+const Header = ({ handleTheme }) => {
+  //this is a helper function that passes a function into setTheme(). The function says take what gets passed to me and set it to the opposite boolean. In this case, it's whatever the theme state was set to. 
+  
+  const toggleTheme = () => {
+    handleTheme((theme) => !theme)
+  }
+
   return (
-    <div className='header'>
+    <div className="header">
       <h1>Sticky Notes</h1>
-      <button className='save'> Toggle </button>
+      <button
+        className="save"
+        onClick={toggleTheme}
+      >
+        Toggle
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+
+export default Header;
+
